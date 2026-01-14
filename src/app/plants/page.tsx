73572177a741206b54e0e3a342078ea6c49cf78e
unknown <you@example.com>
2026-01-14 15:51:42 +0700

    FIst

---
+++ src/app/plants/page.tsx
@@ -0,0 +1,9 @@
+export default function PlantsPage() {
+    return (
+      <main style={{ padding: 20 }}>
+        <h1>🌱 My Plants</h1>
+        <p>หน้านี้ใช้แสดงรายการต้นไม้</p>
+      </main>
+    )
+  }
+  
